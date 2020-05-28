4c9e7c699164a18de48feac38dd30f84fd861388
IsaqueOlliver <ioliveirasp45@gmail.com>
2020-05-28 19:12:17 -0300

    0.0.3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+window.addEventListener("keydown", verifyKey)
+
 function sendMessage() {
     let message = document.querySelector(".send-msg input");
     let msg = message.value;
@@ -7,4 +9,11 @@ function sendMessage() {
     newHistory += `<p>Anyone: ${msg}</p`;
     msgHistory.innerHTML = newHistory;
     message.value = "";
+    message.focus();
+}
+
+function verifyKey(e) {
+	if(e.keyCode == 13) {
+		sendMessage();
+	}
 }
